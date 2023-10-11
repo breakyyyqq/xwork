f7b1e089265dfb70a211bef918015d51acfb7fcc
breakyyyqq <147592346+breakyyyqq@users.noreply.github.com>
2023-10-11 19:48:07 +0300

    First commit

---
+++ matematik(toplama-çıkarma-çarpma-bölme-working1/matematik(toplama-çıkarma-çarpma-bölme-working1/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 15;
+int b = 22;
+int c = 32;
+int toplam = a + b + c;
+Console.WriteLine(toplam);
+int cıkarma = a - b - c;
+Console.WriteLine(cıkarma);
+int carpma = a * b * c;
+Console.WriteLine(carpma);
+int bolme = a / b / c;
+Console.WriteLine(bolme);
